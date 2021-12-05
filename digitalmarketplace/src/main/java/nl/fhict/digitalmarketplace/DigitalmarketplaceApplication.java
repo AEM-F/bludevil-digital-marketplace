fb0a3eef4b6ada5ce2c9a1897729ee714cc4f79e
Alexandru,Emilian E.M <e.alexandru@student.fontys.nl>
2021-12-05 15:16:27 +0100

    Update for GitHub transition

--- digitalmarketplace/src/main/java/nl/fhict/digitalmarketplace/DigitalmarketplaceApplication.java
+++ digitalmarketplace/src/main/java/nl/fhict/digitalmarketplace/DigitalmarketplaceApplication.java
@@ -2,12 +2,12 @@ package nl.fhict.digitalmarketplace;
 
 import com.zaxxer.hikari.HikariDataSource;
 import nl.fhict.digitalmarketplace.config.DatabaseConfig;
-import nl.fhict.digitalmarketplace.model.product.Genre;
-import nl.fhict.digitalmarketplace.model.product.ProductPlatform;
+import nl.fhict.digitalmarketplace.model.product.*;
 import nl.fhict.digitalmarketplace.model.user.ERole;
 import nl.fhict.digitalmarketplace.model.user.Role;
 import nl.fhict.digitalmarketplace.repository.product.GenreRepository;
 import nl.fhict.digitalmarketplace.repository.product.ProductPlatformRepository;
+import nl.fhict.digitalmarketplace.repository.product.ProductRepository;
 import nl.fhict.digitalmarketplace.repository.user.RoleRepository;
 import nl.fhict.digitalmarketplace.service.img.IImageService;
 import nl.fhict.digitalmarketplace.service.img.ImageService;
@@ -21,12 +21,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class DigitalmarketplaceApplication implements CommandLineRunner {
 
+    protected ProductRepository productRepository;
     protected RoleRepository roleRepository;
     protected GenreRepository genreRepository;
     protected ProductPlatformRepository platformRepository;
@@ -57,6 +59,11 @@ public class DigitalmarketplaceApplication implements CommandLineRunner {
     @Autowired
     public void setAdminAccountGenerator(AdminAccountGenerator adminAccountGenerator) {
         this.adminAccountGenerator = adminAccountGenerator;
+    }
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public static void main(String[] args) {
@@ -119,5 +126,50 @@ public class DigitalmarketplaceApplication implements CommandLineRunner {
         this.adminAccountGenerator.buildAdminAccount();
         this.imageService.addImageNotAvailableFile();
 //        insertDataTest();
+//        this.createProductsForDemo();
+    }
+
+    private void createProductsForDemo(){
+        ProductPlatform testPlatform1 = new ProductPlatform(1, "origin");
+        Genre testGenre = new Genre(1, "action");
+        List<Genre> testGenres = new ArrayList<>();
+        testGenres.add(testGenre);
+        Date testLocalDate = new Date();
+        VideoGame product1 = new VideoGame();
+        product1.setName("Resident Evil Village");
+        product1.setProductPlatform(testPlatform1);
+        product1.setQuantity(0);
+        product1.setPrice(50);
+        product1.setDescription("Set a few years after the horrifying events in the critically acclaimed Resident Evil 7 biohazard, the all-new storyline begins with Ethan Winters and his wife Mia living peacefully in a new location, free from their past nightmares. Just as they are building their new life together, tragedy befalls them once again.");
+        product1.setSystemRequirements("TBA");
+        product1.setActive(true);
+        product1.setReleaseDate(testLocalDate);
+        product1.setGenres(testGenres);
+        product1.setImageUrl("https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/04/Village-Frame-Rate-Resolution-1.jpg");
+        productRepository.save(product1);
+
+        VideoGame product2 = new VideoGame();
+        product2.setName("Call of Duty: Black Ops Cold War Green");
+        product2.setProductPlatform(testPlatform1);
+        product2.setQuantity(0);
+        product2.setPrice(40);
+        product2.setDescription("Black Ops Cold War will drop fans into the depths of the Cold War’s volatile geopolitical battle of the early 1980s. Nothing is ever as it seems in a gripping single-player Campaign, where players will come face-to-face with historical figures and hard truths, as they battle around the globe through iconic locales like East Berlin, Vietnam, Turkey, Soviet KGB headquarters and more.");
+        product2.setSystemRequirements("TBA");
+        product2.setActive(true);
+        product2.setReleaseDate(testLocalDate);
+        product2.setGenres(testGenres);
+        product2.setImageUrl("https://images.pushsquare.com/b78d155f4c129/call-of-duty-black-ops-cold-war-ps5-ps4.original.jpg");
+        productRepository.save(product2);
+
+        SoftwareProduct product3 = new SoftwareProduct();
+        product3.setName("Windows 10 Professional OEM Key");
+        product3.setProductPlatform(testPlatform1);
+        product3.setQuantity(0);
+        product3.setPrice(26);
+        product3.setDescription("Windows 10 pro is the latest version of the popular operating system. After the latest updates it presents more of a customized interface that recognizes the device's hardware. Put simply, it’s a system you can use on any type of machine. Furthermore, it is safe and fast: just what any professional, gamer or student needs, especially when complimented with the latest Ms Office package available here.");
+        product3.setSystemRequirements("TBA");
+        product3.setActive(true);
+        product3.setImageUrl("https://msofficeworks.com/wp-content/uploads/2017/06/win-10-pro-64-32-bits-msofficeworks.jpg");
+        productRepository.save(product3);
     }
 }
